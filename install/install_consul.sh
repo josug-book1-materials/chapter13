67f2599651fd08ab5872f26dad26da39900187e7
Masahito Zembutsu <m.zembutsu@gmail.com>
2014-12-15 12:33:48 +0900

    update

--- install/install_consul.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget -O 0.4.1_linux_amd64.zip https://dl.bintray.com/mitchellh/consul/0.4.1_linux_amd64.zip
-unzip ./0.4.1_linux_amd64.zip
-mv ./consul /usr/local/bin/consul
--- install/install_serf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-wget -O 0.6.3_linux_amd64.zip https://dl.bintray.com/mitchellh/serf/0.6.3_linux_amd64.zip
-unzip ./0.6.3_linux_amd64.zip
-mv ./serf /usr/local/bin/serf
-
